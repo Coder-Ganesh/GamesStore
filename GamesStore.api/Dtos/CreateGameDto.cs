04380a4cf38929df690dcdef3dc9f863820f6f53
Shriganesh Singh <gsacpce@hotmail.com>
2025-01-21 18:21:10 +0530

    With Nuget validations

---
+++ GamesStore.api/Dtos/CreateGameDto.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GamesStore.api.Dtos;
+
+public record class CreateGameDto(
+    [Required][StringLength(50)]string Name,
+    [Required][StringLength(50)]string Genre,
+    [Range(1,100)]decimal Price,
+    DateOnly ReleaseDate);
