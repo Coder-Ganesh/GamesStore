2cd5219c85e170cd126b8bb1aa3bfc6685d9c83a
Shriganesh Singh <gsacpce@hotmail.com>
2025-02-14 18:44:35 +0530

    Mapping entities to DTOs

---
+++ GamesStore.api/EndPoints/GamesEndpoints.cs
@@ -0,0 +1,68 @@
+using System;
+using GamesStore.api.Dtos;
+
+namespace GamesStore.api.EndPoints;
+
+public static class GamesEndpoints
+{
+   const string GetGameEndpointName = "GetGame";
+
+        private static readonly List<GameDto> games = [
+            new(1,"Street Fighter II","Fighting",19.99M,new DateOnly(1992,7,15)),
+            new(2,"Final Fantasy XIV","Role Playing",59.99M,new DateOnly(2010,9,30)),
+            new(3,"FIFA 23","Sports",69.99M,new DateOnly(2022,9,27))
+        ];
+
+
+        public static RouteGroupBuilder MapGamesEndPoints(this WebApplication app)
+        {
+            var group = app.MapGroup("Games").WithParameterValidation();
+
+             // GET /Games
+            group.MapGet("/", () => games);
+
+            // GET /Games/1
+            group.MapGet("/{id}/", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
+
+            // POST /Games
+            group.MapPost("/", (CreateGameDto newGame) =>
+            {               
+                GameDto game = new(
+                games.Count + 1, newGame.Name, newGame.Genre, newGame.Price, newGame.ReleaseDate);
+
+                games.Add(game);
+
+                return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+            });
+
+            // PUT /Games
+            group.MapPut("/{id}", (int id, UpdateGameDto updateGame) =>
+            {
+                var index = games.FindIndex(game => game.Id == id);
+
+                games[index] = new GameDto(
+                    id,
+                    updateGame.Name,
+                    updateGame.Genre,
+                    updateGame.Price,
+                    updateGame.ReleaseDate
+                );
+
+                return Results.NoContent;
+            });
+
+
+            // DELETE/Games/1
+            group.MapDelete("/{id}" , (int id) => 
+            {
+                games.RemoveAll(game => game.Id == id);
+
+                return Results.NotFound();
+            });
+
+            //app.MapGet("/", () => "Hello World!");
+
+            return group;
+
+        }
+}
--- GamesStore.api/Program.cs
+++ GamesStore.api/Program.cs
@@ -1,9 +1,21 @@
 
 using GamesStore.api.EndPoints;
+using GameStore.api.Data;
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using GamesStore.api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+        var connString = builder.Configuration.GetConnectionString("GameStore");
+        //builder.Services.AddSqlite<GameStoreContext>(connString);
+        builder.Services.AddSqlite<GameStoreContext>(connString);
+
         var app = builder.Build();
 
         app.MapGamesEndPoints();
+
+        app.MigrateDb();
 
         app.Run();
